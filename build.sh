f343d97cf172d618c3ee871a743a96a3060526df
yubaoliu <yubaoliu89@gmail.com>
2020-08-25 21:37:16 +0900

    Run ROS wrapper

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 echo "Configuring and building Thirdparty/DBoW2 ..."
 
 cd Thirdparty/DBoW2
+rm build -rf
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
@@ -10,6 +11,7 @@ cd ../../g2o
 
 echo "Configuring and building Thirdparty/g2o ..."
 
+rm build -rf
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
@@ -26,6 +28,7 @@ cd ..
 echo "Configuring and building ORB_SLAM3 ..."
 
 mkdir build
+rm build -rf
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
 make -j2
